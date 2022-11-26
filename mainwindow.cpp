981022a741d151b77f3213271c386f0c776f9db4
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2022-11-26 17:33:06 +0200

    mainwindow: remove the outer axis of the plot and add inner axis

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,25 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QwtPlotScaleItem>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Remove the outer axis
+    ui->qwtPlot->enableAxis(QwtAxis::YLeft, false);
+    ui->qwtPlot->enableAxis(QwtAxis::XBottom, false);
+    // Set the axis scale
+    ui->qwtPlot->setAxisScale(QwtAxis::YLeft, -90.0, 90.0);
+    ui->qwtPlot->setAxisScale(QwtAxis::XBottom, -90.0, 90.0);
+
+    // Add inner axis
+    QwtPlotScaleItem *verticalAxisScale = new QwtPlotScaleItem(QwtScaleDraw::RightScale, 0.0);
+    verticalAxisScale->attach(ui->qwtPlot);
+    QwtPlotScaleItem *horizontalAxisScale = new QwtPlotScaleItem(QwtScaleDraw::BottomScale, 0.0);
+    horizontalAxisScale->attach(ui->qwtPlot);
 }
 
 MainWindow::~MainWindow()
